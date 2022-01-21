690e647f79bb6146a91300237a0d35352265a16f
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-01-21 09:50:08 +0800

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Giovanni Gaffé on 2022/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
