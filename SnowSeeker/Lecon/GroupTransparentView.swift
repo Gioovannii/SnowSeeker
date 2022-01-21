98ec729c1a1f9bc79166a278f7f4f42256220b63
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-01-21 11:02:45 +0800

    Set file for group transparent view

---
+++ SnowSeeker/Lecon/GroupTransparentView.swift
@@ -0,0 +1,40 @@
+//
+//  GroupTransparentView.swift
+//  SnowSeeker
+//
+//  Created by Giovanni Gaffé on 2022/1/21.
+//
+
+import SwiftUI
+
+struct UserView: View {
+    var body: some View {
+        Group {
+            Text("Name: Paul")
+            Text("Country: England")
+            Text("Pets: Luna, Arya, and Toby")
+        }
+    }
+}
+
+struct GroupTransparentView: View {
+    @Environment(\.horizontalSizeClass) var sizeClass
+    @State private var layoutVertically = false
+    
+    var body: some View {
+        Group {
+            if sizeClass == .compact {
+                VStack(content: UserView.init)
+            
+            } else {
+                HStack(content: UserView.init)
+            }
+        }
+    }
+}
+
+struct GroupTransparentView_Previews: PreviewProvider {
+    static var previews: some View {
+        GroupTransparentView()
+    }
+}
