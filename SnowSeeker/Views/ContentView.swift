ad96817bbbf7a9f9c3908544436ea223f3023360
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-01-24 12:25:35 +0800

    add content vie file to group views

---
+++ SnowSeeker/Views/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  SnowSeeker
+//
+//  Created by Giovanni Gaffé on 2022/1/21.
+//
+
+import SwiftUI
+
+extension View {
+    @ViewBuilder func phoneOnlyNavigationView() -> some View {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            self.navigationViewStyle(.stack)
+        } else {
+            self
+        }
+    }
+}
+struct ContentView: View {
+    let resorts: [Resort] = Bundle.main.decode("resorts.json")
+    
+    var body: some View {
+        NavigationView {
+            List(resorts) { resort in
+                
+                NavigationLink {
+                    ResortView(resort: resort)
+                    
+                } label: {
+                    Image(resort.country)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 25)
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(.black, lineWidth: 1))
+                    
+                    VStack(alignment: .leading) {
+                        Text(resort.name)
+                            .font(.headline)
+                        Text("\(resort.runs) runs")
+                            .foregroundColor(.secondary)
+                    }
+                }
+                
+            }
+            .navigationTitle("Resorts")
+
+            WelcomeView()
+        }
+//        .phoneOnlyNavigationView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
