b05184364f7f7295aaa74e05b51192707ec20573
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-01-26 10:23:53 +0800

    update facility for iOS 15

--- SnowSeeker/Model/Facility.swift
+++ SnowSeeker/Model/Facility.swift
@@ -11,39 +11,37 @@ struct Facility: Identifiable {
     let id = UUID()
     var name: String
     
+    private let icons = [
+        "Accommodation": "house",
+        "Beginners": "1.circle",
+        "Cross-country": "map",
+        "Eco-friendly": "leaf.arrow.circlepath",
+        "Family": "person.3"
+    ]
+    
+    private let descriptions = [
+        "Accommodation": "This resort has popular on-site accommodation.",
+        "Beginners": "This resort has lots of ski schools.",
+        "Cross-country": "This resort has many cross-country ski routes.",
+        "Eco-friendly": "This resort has won an award for environmental friendliness.",
+        "Family": "This resort is popular with families."
+    ]
+    
     var icon: some View {
-        let icons = [
-            "Accommodation": "house",
-            "Beginners": "1.circle",
-            "Cross-country": "map",
-            "Eco-friendly": "leaf.arrow.circlepath",
-            "Family": "person.3"
-        ]
-        
         if let iconName = icons[name] {
-            let image = Image(systemName: iconName)
-                .accessibilityLabel(Text(name))
+            return Image(systemName: iconName)
+                .accessibilityLabel(name)
                 .foregroundColor(.secondary)
-            return image
         } else {
             fatalError("Unknown facility type: \(name)")
         }
     }
     
-    var alert: Alert {
-        let messages = [
-            "Accommodation": "This resort has popular on-site accommodation.",
-            "Beginners": "This resort has lots of ski schools.",
-            "Cross-country": "This resort has many cross-country ski routes.",
-            "Eco-friendly": "This resort has won an award for environmental friendliness.",
-            "Family": "This resort is popular with families."
-        ]
-        
-        if let message = messages[name] {
-            return Alert(title: Text(name), message: Text(message))
-            
+    var description: String {
+        if let message = descriptions[name] {
+            return message
         } else {
-            fatalError("Unknown facility type \(name)")
+            fatalError("Unknown facility type: \(name)")
         }
     }
 }
