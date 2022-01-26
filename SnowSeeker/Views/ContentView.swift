00c9eaf5677ecf5ed8e6ff109e6d729546275de3
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-01-26 11:08:37 +0800

    update project for iOS 15

--- SnowSeeker/Views/ContentView.swift
+++ SnowSeeker/Views/ContentView.swift
@@ -18,7 +18,7 @@ extension View {
 }
 
 struct ContentView: View {
-    @ObservedObject var favorites = Favorites()
+    @StateObject var favorites = Favorites()
     let resorts: [Resort] = Bundle.main.decode("resorts.json")
     
     @State private var searchText = ""
@@ -31,28 +31,29 @@ struct ContentView: View {
                     ResortView(resort: resort)
                     
                 } label: {
-                    Image(resort.country)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 40, height: 25)
-                        .clipShape(RoundedRectangle(cornerRadius: 5))
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 5)
-                                .stroke(.black, lineWidth: 1))
-                    
-                    VStack(alignment: .leading) {
-                        Text(resort.name)
-                            .font(.headline)
-                        Text("\(resort.runs) runs")
-                            .foregroundColor(.secondary)
-                    }
-                    .layoutPriority(1)
-                    
-                    if favorites.contains(resort) {
-                        Spacer()
-                        Image(systemName: "heart.fill")
-                            .accessibilityLabel(Text("This is a favorite resort"))
-                            .foregroundColor(.red)
+                    HStack {
+                        Image(resort.country)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 40, height: 25)
+                            .clipShape(RoundedRectangle(cornerRadius: 5))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(.black, lineWidth: 1))
+                        
+                        VStack(alignment: .leading) {
+                            Text(resort.name)
+                                .font(.headline)
+                            Text("\(resort.runs) runs")
+                                .foregroundColor(.secondary)
+                        }
+                        
+                        if favorites.contains(resort) {
+                            Spacer()
+                            Image(systemName: "heart.fill")
+                                .accessibilityLabel("This is a favorite resort")
+                                .foregroundColor(.red)
+                        }
                     }
                 }
             }
